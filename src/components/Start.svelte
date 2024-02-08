ff6aa63e058609d003150c4b23b8f246c9a568eb
FerreiraG <gabriel.ferreira@etu.univ-orleans.fr>
2024-02-08 12:23:11 +0100

    fix color and formula one image

--- src/components/Start.svelte
+++ src/components/Start.svelte
@@ -10,7 +10,8 @@
   >
     <h2>Challenge du Quizz de Formule 1</h2>
     <p>
-      Essayez de répondre aux questions liées à la formule 1 dans la limite de temps.
+      Essayez de répondre aux questions liées à la formule 1 dans la limite de
+      temps.
     </p>
     <p>
       Gardez à l'esprit que des réponses incorrectes pénaliseront votre
@@ -18,6 +19,13 @@
     </p>
 
     <button on:click>Commencer le quizz</button>
+  </div>
+  <div class="formule1center">
+    <img
+      src="https://gp-explorer.fr/wp-content/themes/nc/static/svg/formule-4-bleu.svg"
+      class="formule-4 js-reveal hidden m:block absolute -top-6 left-[68%] m:w-[280px] l:w-[354px] is-visible"
+      alt=""
+    />
   </div>
 </section>
 
